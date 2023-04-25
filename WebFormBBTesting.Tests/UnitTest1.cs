b01b2404d55cf8e4868d36478d1076fd3432414d
Seafra O'Grady <S00165540@mail.itsligo.ie>
2023-04-25 13:19:31 +0100

    updated tests. still connection error

--- WebFormBBTesting.Tests/UnitTest1.cs
+++ WebFormBBTesting.Tests/UnitTest1.cs
@@ -8,127 +8,162 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace WebFormBBTesting
+namespace WebFormBBTesting.Tests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class UnitTest1
     {
         private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
+    private StringBuilder verificationErrors;
+    private string baseURL;
+    private bool acceptNextAlert = true;
+        
 
-        [SetUp]
-        public void SetupTest()
-        {
-            driver = new ChromeDriver();
-            baseURL = "https://www.google.com/";
-            verificationErrors = new StringBuilder();
-        }
+    [SetUp]
+    public void SetupTest()
+    {
+        driver = new ChromeDriver();
+        baseURL = "https://www.google.com/";
+        verificationErrors = new StringBuilder();
+    }
 
-        [TearDown]
-        public void TeardownTest()
+    [TearDown]
+    public void TeardownTest()
+    {
+        try
         {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
+            driver.Quit();
         }
+        catch (Exception)
+        {
+            // Ignore errors if unable to close the browser
+        }
+        Assert.AreEqual("", verificationErrors.ToString());
+    }
 
-        [Test]
-        public void TheUntitledTestCaseTest()
-        {
-            driver.Navigate().GoToUrl("https://localhost:44389/");
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("25");
-            driver.FindElement(By.Id("MainContent_TextBox2")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Rural");
-            driver.FindElement(By.Id("MainContent_Button1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("");
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("35");
-            driver.FindElement(By.Id("MainContent_Button1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("40");
-            driver.FindElement(By.Id("MainContent_TextBox2")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Urban");
-            driver.FindElement(By.Id("MainContent_Button1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("50");
-            driver.FindElement(By.Id("MainContent_Button1")).Click();
-            driver.FindElement(By.XPath("//form[@id='ctl01']/div[3]")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("15");
-            driver.FindElement(By.Id("MainContent_TextBox2")).Click();
-            driver.FindElement(By.Id("MainContent_Button1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("17");
-            driver.FindElement(By.Id("MainContent_TextBox2")).Click();
-            driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
-            driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("rural");
-            driver.FindElement(By.Id("MainContent_Button1")).Click();
-        }
+    [Test]
+    public void The25RuralTest()
+    {
+        driver.Navigate().GoToUrl("https://localhost:42800/");
+        driver.FindElement(By.Id("MainContent_TextBox1")).Click();
+        driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+        driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("25");
+        driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
+        driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Rural");
+        string five = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+        // ERROR: Caught exception [unknown command []]
+    }
+       [Test]
+       public void The35RuralTest()
+       {
+           driver.Navigate().GoToUrl("https://localhost:42800/");
+           driver.FindElement(By.Id("MainContent_TextBox1")).Click();
+           driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("35");
+           String threePointFive = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+       }
+
+       [Test]
+       public void The25UrbanTest()
+       {
+           driver.Navigate().GoToUrl("https://localhost:42800/");
+           driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("25");
+           driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Urban");
+           driver.FindElement(By.Id("MainContent_Button1")).Click();
+           String six = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+       }
+       [Test]
+       public void The40UrbanTest()
+       {
+           driver.Navigate().GoToUrl("https://localhost:42800/");
+           driver.FindElement(By.Id("MainContent_TextBox1")).Click();
+           driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("40");
+           driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Urban");
+           String five = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+       }
+
+
+       [Test]
+       public void The50RuralTest()
+       {
+           driver.Navigate().GoToUrl("https://localhost:42800/");
+           driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("50");
+           driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Rural");
+           String discounted = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+       }
+       [Test]
+       public void The15RuralTest()
+       {
+           driver.Navigate().GoToUrl("https://localhost:42800/");
+           driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("50");
+           driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Rural");
+           String zero = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+       }
+       [Test]
+       public void The17UrbanTest()
+       {
+           driver.Navigate().GoToUrl("https://localhost:42800/");
+           driver.FindElement(By.Id("MainContent_TextBox1")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox1")).SendKeys("17");
+           driver.FindElement(By.Id("MainContent_TextBox2")).Clear();
+           driver.FindElement(By.Id("MainContent_TextBox2")).SendKeys("Urban");
+           String zero = driver.FindElement(By.Id("MainContent_Button1")).GetAttribute("value");
+       }
+
         private bool IsElementPresent(By by)
+    {
+        try
         {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
+            driver.FindElement(by);
+            return true;
         }
-
-        private bool IsAlertPresent()
+        catch (NoSuchElementException)
         {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
+            return false;
         }
     }
+
+    private bool IsAlertPresent()
+    {
+        try
+        {
+            driver.SwitchTo().Alert();
+            return true;
+        }
+        catch (NoAlertPresentException)
+        {
+            return false;
+        }
+    }
+
+    private string CloseAlertAndGetItsText()
+    {
+        try
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            string alertText = alert.Text;
+            if (acceptNextAlert)
+            {
+                alert.Accept();
+            }
+            else
+            {
+                alert.Dismiss();
+            }
+            return alertText;
+        }
+        finally
+        {
+            acceptNextAlert = true;
+        }
+    }
+}
 }
